994d3a98e0f851af2593f68f6eec0e691693459e
和田健嗣 <ponporopen@gmail.com>
2020-06-21 11:54:22 +0900

    rest のサンプル作成

---
+++ src/app.module.ts
@@ -0,0 +1,12 @@
+import { Module } from '@nestjs/common'
+import { AppController } from './app.controller'
+import { AppService } from './app.service'
+import { TestController } from './test/test.controller'
+import { TestService } from './test/test.service'
+
+@Module({
+  imports: [],
+  controllers: [AppController, TestController],
+  providers: [AppService, TestService],
+})
+export class AppModule {}
